5305a7598f34bbf447ea4dd44204f1a04290ba6c
Siddhant <me@siddhantgupta.in>
2024-02-26 12:46:26 +0530

    Updated  the about page

--- src/Components/AboutMe/AboutMe.jsx
+++ src/Components/AboutMe/AboutMe.jsx
@@ -1,10 +1,15 @@
-import style from "./AboutME.module.css";
+import style from "./AboutMe.module.css";
 
 const AboutME = () => {
     return(
         <>
             <div className={style.container}>
-                AboutME
+            <div className={style.heading}>
+                ABOUT ME
+            </div>
+            <div className={style.paragraph}>
+            A highly innovative individual with a keen interest in developing and deploying new projects on World Wide Web. Ability to integrate market needs into technical solutions. 
+            </div>
             </div>
         </>
     );
